7a3f3c38ba1fbdad13bc102e3933f5d6e8927a0c
Asma Ali <128274795+AsmaAl12@users.noreply.github.com>
2023-07-29 00:21:54 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+//============================JS CLASS 3====================//
+
+
+//=====OPERATORS=====//
+// Assignment Operators => (= , += , -== , *= , /= , %= , **=)
+// Logical Operators => (&& ,|| , !)
+// Arithmetic Operators => (+ , - , / , * , ** , % , ++ , --)
+// Comparison Operators => (== , === , < , > , != , !== , <= , >= , ?)
+
+
+//=====FUNCTIONS=====//
+// function checkAge() {
+//     console.log();
+// }
+// checkAge(10);
+// console.log(typeof(checkAge));
